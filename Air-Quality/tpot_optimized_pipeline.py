5ee9492c50297aed55de865d45eb513016bfa6c3
Manoj Manivannan <manojm18@live.in>
2021-10-16 22:38:10 +0200

    update tpot air quality notebook

--- Air-Quality/tpot_optimized_pipeline.py
+++ Air-Quality/tpot_optimized_pipeline.py
@@ -1,7 +1,12 @@
 import numpy as np
 import pandas as pd
+from sklearn.ensemble import ExtraTreesRegressor
+from sklearn.linear_model import LassoLarsCV
 from sklearn.model_selection import train_test_split
-from xgboost import XGBRegressor
+from sklearn.pipeline import make_pipeline, make_union
+from sklearn.preprocessing import PolynomialFeatures
+from tpot.builtins import StackingEstimator
+from sklearn.impute import SimpleImputer
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -9,8 +14,17 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=None)
 
-# Average CV score on the training set was: -0.00048372641085816473
-exported_pipeline = XGBRegressor(learning_rate=0.1, max_depth=10, min_child_weight=2, n_estimators=100, n_jobs=1, objective="reg:squarederror", subsample=0.4, verbosity=0)
+imputer = SimpleImputer(strategy="median")
+imputer.fit(training_features)
+training_features = imputer.transform(training_features)
+testing_features = imputer.transform(testing_features)
+
+# Average CV score on the training set was: -0.0003385226905287119
+exported_pipeline = make_pipeline(
+    StackingEstimator(estimator=ExtraTreesRegressor(bootstrap=False, max_features=0.05, min_samples_leaf=6, min_samples_split=9, n_estimators=100)),
+    PolynomialFeatures(degree=2, include_bias=False, interaction_only=False),
+    LassoLarsCV(normalize=False)
+)
 
 exported_pipeline.fit(training_features, training_target)
 results = exported_pipeline.predict(testing_features)
